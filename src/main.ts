94e9b33877ba494359d1cea59d175618e3de5cb4
Eugene <zhenyaskryaga@gmail.com>
2026-02-13 22:19:41 +0200

    homework 36

---
+++ src/main.ts
@@ -0,0 +1,137 @@
+// Task #1
+
+let userName: string;
+let userAge: number;
+let isStudent: boolean;
+let emptyValue: null;
+let notAssigned: undefined;
+
+// Task #2
+
+let something: unknown;
+something = "string";
+something = "55";
+something = true;
+
+// Task #3
+
+function printValue(arg: unknown): void {
+  if (typeof arg === "string") {
+    console.log(arg.length);
+  } else if (typeof arg === "number") {
+    console.log(arg ** 2);
+  } else {
+    console.log("Невідомий тип");
+  }
+}
+
+// Task #4
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+  phone?: string;
+}
+
+const user1: User = {
+  id: 1,
+  name: "Eugene",
+  email: "eugene@gmail.com",
+  isActive: true,
+};
+
+// Task #5
+
+const user2: User = {
+  id: 2,
+  name: "notEugene",
+  email: "noteugene@gmail.com",
+  isActive: false,
+};
+
+// Task #6
+
+interface Product {
+  title: string;
+  price: number;
+  tags: string[];
+}
+
+const phone: Product = {
+  title: "phone",
+  price: 200,
+  tags: ["tag1", "tag2", "tag3"],
+};
+
+const laptop: Product = {
+  title: "laptop",
+  price: 500,
+  tags: ["tag1", "tag2"],
+};
+
+const products: Product[] = [phone, laptop];
+
+// Task #7
+
+let id: number | string;
+
+id = 100;
+id = "100";
+
+// Task #8
+
+function formatID(id: number | string) {
+  return typeof id === "number" ? "ID: 123" : "ID: abc";
+}
+
+// Task #9
+
+type Status = "success" | "error" | "loading";
+
+const successStatus: Status = "success";
+
+// Task #10
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+const strings: string[] = ["a", "b", "c"];
+const flags: boolean[] = [true, false, true];
+
+// Task #11
+
+const users: User[] = [user1, user2];
+
+// Task #12
+
+const mixedArray: (string | number)[] = ["one", 1, "1", 111];
+
+// Task #13
+
+function sum(first: number, second: number): number {
+  return first + second;
+}
+
+// Task #14
+
+function greet(name: string): void {
+  console.log(`Hello, ${name}!`);
+}
+
+// Task #15
+
+function getDiscount(price: number, discount: number) {
+  return price - (price * discount) / 100;
+}
+
+// Task #16
+
+function handleResponse(status: "success" | "error", data: unknown) {
+  if (status === "success" && typeof data === "string") {
+    return data.length;
+  } else if (status === "success" && typeof data === "number") {
+    return data * 2;
+  } else if (status === "error") {
+    return "Something went wrong";
+  }
+}
